fb9ec7ffa48b7d336d26fb3d21f7227ce58b659c
Sahil Khairnar <sahilkhairnar2786@gmail.com>
2025-09-15 00:26:14 +0530

    update CORS configuration to allow requests from Vercel frontend

--- src/app.js
+++ src/app.js
@@ -6,12 +6,16 @@ const app = express();
 // ✅ CORS setup
 app.use(
     cors({
-        origin: "http://localhost:5173", // your React app’s URL
+        origin: [
+            "http://localhost:5173",
+            "https://image-r85d.vercel.app", // your Vercel frontend
+        ],
         methods: ["GET", "POST", "PUT", "DELETE"],
         allowedHeaders: ["Content-Type", "Authorization"],
-        credentials: true
+        credentials: true,
     })
 );
+
 // Middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
